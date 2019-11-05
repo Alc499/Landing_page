ac89e08816c407254226bbe0f482534c71e24aac
Alisa Vitchenko <alisa.vitchenko@dev-pro.net>
2019-11-05 17:56:24 +0200

    Updates

---
+++ precache-manifest.7a7e91a616bd24a12a408097a670ba84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b43b99c0f85b24af4ad470c9812add3",
+    "url": "/Landing_page/./index.html"
+  },
+  {
+    "revision": "540bd6e0ef74bcd50e69",
+    "url": "/Landing_page/./static/css/main.5dadb683.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "/Landing_page/./static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "540bd6e0ef74bcd50e69",
+    "url": "/Landing_page/./static/js/main.434b6a99.chunk.js"
+  },
+  {
+    "revision": "c366f4324ebd736f8e02",
+    "url": "/Landing_page/./static/js/runtime~main.4e8e9690.js"
+  }
+]);
